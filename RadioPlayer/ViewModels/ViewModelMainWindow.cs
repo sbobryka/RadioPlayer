1aca7ca56d27825151cbf5d04eb952844dcda578
Сергей Бобрыка <sbobryka@outlook.com>
2020-10-13 12:04:49 +0300

    Добавил команды добавления и удаления станций

--- RadioPlayer/ViewModels/ViewModelMainWindow.cs
+++ RadioPlayer/ViewModels/ViewModelMainWindow.cs
@@ -35,30 +35,58 @@ namespace RadioPlayer.ViewModels
 
         #endregion
 
-        #region Команды
+        #region Командa - добавить
 
-        public ICommand TestCommand { get; }
+        public ICommand AddStationCommand { get; }
 
-        private void OnTestCommandExecuted(object property)
+        private void OnAddStationExecuted(object property)
         {
-
+            Stations.Add(new Station("Новая станция", @"http:\\"));
         }
 
-        private bool CanTestCommandExecut(object property) => true;
+        private bool CanAddStationExecute(object property) => true;
 
         #endregion
 
+        #region Командa - удалить
+
+        public ICommand RemoveStationCommand { get; }
+
+        private void OnRemoveStationExecuted(object property)
+        {
+            Stations.Remove(property as Station);
+            SelectedStation = Stations.FirstOrDefault();
+        }
+
+        private bool CanRemoveStationExecute(object property) => Stations.Count > 0;
+
+        #endregion
+
+        #region Список станций
+
         public ObservableCollection<Station> Stations { get; set; }
+
+        #endregion
+
+        #region Конструктор
 
         public ViewModelMainWindow()
         {
             Stations = new ObservableCollection<Station>()
             {
                 new Station("Record", @"http:\\record.ru"),
-                new Station("Energy", @"http:\\energy.ru")
+                new Station("Energy", @"http:\\energy.ru"),
+                new Station("Авто радио", @"http:\\avtoradio.ru")
             };
 
+            Stations = new ObservableCollection<Station>(Stations.OrderBy(s => s.Name));
+
             SelectedStation = Stations.FirstOrDefault();
+
+            AddStationCommand = new RelayCommand(OnAddStationExecuted, CanAddStationExecute);
+            RemoveStationCommand = new RelayCommand(OnRemoveStationExecuted, CanRemoveStationExecute);
         }
+
+        #endregion
     }
 }
