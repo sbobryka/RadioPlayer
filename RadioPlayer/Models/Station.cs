dc17ac86e65e22c2364c7a8a2bb8a2b99fe97e51
Сергей Бобрыка <sbobryka@outlook.com>
2020-10-13 10:17:42 +0300

    Создал модель станции

---
+++ RadioPlayer/Models/Station.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadioPlayer.Models
+{
+    internal class Station
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Station(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
