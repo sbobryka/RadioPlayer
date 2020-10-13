ab55adb0d4bb7bcadf337f9ea9f50564a0967403
Сергей Бобрыка <sbobryka@outlook.com>
2020-10-13 13:51:44 +0300

    Добавил проигрыватель и команду играть

--- RadioPlayer/ViewModels/ViewModelMainWindow.cs
+++ RadioPlayer/ViewModels/ViewModelMainWindow.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace RadioPlayer.ViewModels
 {
     internal class ViewModelMainWindow : ViewModelBase
     {
+        public MediaPlayer MediaPlayer { get; } = new MediaPlayer();
+
         #region Статус
 
         private string status = "Готов";
@@ -41,7 +45,7 @@ namespace RadioPlayer.ViewModels
 
         private void OnAddStationExecuted(object property)
         {
-            Stations.Add(new Station("Новая станция", @"http:\\"));
+            Stations.Add(new Station("Новая станция", @"http://"));
         }
 
         private bool CanAddStationExecute(object property) => true;
@@ -62,6 +66,27 @@ namespace RadioPlayer.ViewModels
 
         #endregion
 
+        #region Командa - играть
+
+        public ICommand PlayStationCommand { get; }
+
+        private void OnPlayStationExecuted(object property)
+        {
+            try
+            {
+                MediaPlayer.Open(new Uri(((Station)property).Address));
+                MediaPlayer.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool CanPlayStationExecute(object property) => SelectedStation != null;
+
+        #endregion
+
         #region Список станций
 
         public ObservableCollection<Station> Stations { get; set; }
@@ -74,17 +99,34 @@ namespace RadioPlayer.ViewModels
         {
             Stations = new ObservableCollection<Station>()
             {
-                new Station("Record", @"http:\\record.ru"),
-                new Station("Energy", @"http:\\energy.ru"),
-                new Station("Авто радио", @"http:\\avtoradio.ru")
+                new Station("Европа Плюс", @"http://ep128.hostingradio.ru:8030/ep128"),
+                new Station("DiFM", @"https://dfm.hostingradio.ru/dfm128.mp3"),
             };
 
             Stations = new ObservableCollection<Station>(Stations.OrderBy(s => s.Name));
 
             SelectedStation = Stations.FirstOrDefault();
 
+            MediaPlayer.MediaFailed += MediaPlayer_MediaFailed;
+            MediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+
             AddStationCommand = new RelayCommand(OnAddStationExecuted, CanAddStationExecute);
             RemoveStationCommand = new RelayCommand(OnRemoveStationExecuted, CanRemoveStationExecute);
+            PlayStationCommand = new RelayCommand(OnPlayStationExecuted, CanPlayStationExecute);
+        }
+
+        #endregion
+
+        #region События подключения
+
+        private void MediaPlayer_MediaFailed(object sender, ExceptionEventArgs e)
+        {
+            Status = e.ErrorException.Message;
+        }
+
+        private void MediaPlayer_MediaOpened(object sender, EventArgs e)
+        {
+            Status = "Успешное открытие мультимедиа.";
         }
 
         #endregion
