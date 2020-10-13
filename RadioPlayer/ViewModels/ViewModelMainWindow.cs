2974fb03fa7605783d58711d6a6c0e733373b58b
Сергей Бобрыка <sbobryka@outlook.com>
2020-10-13 10:52:49 +0300

    Создал вью модели

---
+++ RadioPlayer/ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,52 @@
+﻿using RadioPlayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace RadioPlayer.ViewModels
+{
+    internal class ViewModelMainWindow : ViewModelBase
+    {
+        #region Статус
+
+        private string status = "Готов";
+
+        public string Status
+        {
+            get => status;
+            set => Set(ref status, value);
+        }
+
+        #endregion
+
+        #region Выбранная станция
+
+        private Station selectedStation;
+
+        public Station SelectedStation
+        {
+            get => selectedStation;
+            set => Set(ref selectedStation, value);
+        }
+
+        #endregion
+
+        public ObservableCollection<Station> Stations { get; set; }
+
+        public ViewModelMainWindow()
+        {
+            Stations = new ObservableCollection<Station>()
+            {
+                new Station("Record", @"http:\\record.ru"),
+                new Station("Energy", @"http:\\energy.ru")
+            };
+
+            if (Stations.Count > 0)
+            {
+                SelectedStation = Stations.First();
+            }
+        }
+    }
+}
